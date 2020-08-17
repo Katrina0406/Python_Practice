9af4c4a3b321893dc14f8d4121b776bfcd111bd8
Yuqiao Hu <53132886+Katrina0406@users.noreply.github.com>
2020-08-17 11:10:15 +0800

    Stack code

---
+++ stack_code.py
@@ -0,0 +1,20 @@
+
+# stack: last in, first out(from right)
+
+# TODO: create a new empty stack
+stack = []
+
+# TODO: push items onto the stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+# TODO: print the stack contents
+print(stack)
+
+# TODO: pop an item off the stack (last one)
+x = stack.pop()
+print(x)
+print(stack)
+
