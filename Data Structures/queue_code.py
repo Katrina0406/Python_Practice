494d70a60a6872534d3d924b5eda665130578d52
Yuqiao Hu <53132886+Katrina0406@users.noreply.github.com>
2020-08-17 12:25:46 +0800

    Add files via upload

---
+++ Data Structures/queue_code.py
@@ -0,0 +1,20 @@
+# queue: last in, last out(from left)
+
+from collections import deque
+
+# TODO: create a new empty deque object that will function as a queue
+queue = deque()
+
+# TODO: add some items to the queue
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+
+# TODO: print the queue contents
+print(queue)
+
+# TODO: pop an item off the front of the queue
+x = queue.popleft()
+print(x)
+print(queue)
