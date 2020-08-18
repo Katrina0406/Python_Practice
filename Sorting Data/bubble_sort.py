44d3c7de3593c98bce6837795568f3336ebb8b1f
Yuqiao Hu <53132886+Katrina0406@users.noreply.github.com>
2020-08-18 14:36:18 +0800

    Add files via upload

---
+++ Sorting Data/bubble_sort.py
@@ -0,0 +1,20 @@
+
+
+def bubbleSort(dataset):
+    # TODO: start with the array length and decrement each time
+    # Time Complexity: O(n^2)
+    for i in range(len(dataset)-1, 0, -1):
+        for j in range(i):
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp
+        print("Current state: ", dataset)
+
+def main():
+    list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+    bubbleSort(list1)
+    print("Result: ", list1)
+
+if __name__ == "__main__":
+    main()
