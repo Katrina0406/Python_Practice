4024ef9a17bd9f83fdfe65af792d9db6558803de
Yuqiao Hu <53132886+Katrina0406@users.noreply.github.com>
2020-08-25 16:23:06 +0800

    Add files via upload

---
+++ Work with Files/XML_data.py
@@ -0,0 +1,25 @@
+
+# parsing and processing XML
+# note that the file needed in this program is absent
+
+def main():
+    # use the parse() function to load and parse an XML file
+    doc = xml.dom.minidom.parse("samplexml.xml")
+
+    # print out the document node and the name of the first child tag
+    print(doc.nodeName)
+    print(doc.firstChild.tagName)
+
+    # get a list of XML tags from the document and print each one
+    skills = doc.getElementByTagName("skill")
+    print("%d skills: " % skills.length)
+    for skill in skills:
+        print(skill.getAttribute("name"))
+
+    # create a new XML tag and add it into the document
+    newSkill = doc.createElement("skill")
+    newSkill.setAttribute("name", "jQuery")
+    doc.firstChild.appendChild(newSkill)
+
+if __name__ == '__main__':
+    main()
