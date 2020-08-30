4146d11d6d7c4f2aade2ae45b5ea0f564e10ffb1
Yuqiao Hu <53132886+Katrina0406@users.noreply.github.com>
2020-08-30 21:31:27 +0800

    Add files via upload

---
+++ Data Structures/hash_table.py
@@ -0,0 +1,38 @@
+
+# filtering duplicates
+items = ['apple', 'pear', 'orange', 'banana', 'apple', 'orange',
+        'apple', 'pear', 'banana', 'orange', 'apple', 'kiwi',
+        'pear', 'apple', 'orange']
+
+# TODO: create a hashtable to perform a filter
+filter = dict()
+
+# TODO: loop over each time and add to the hashtable
+for key in items:
+    filter[key] = 0
+
+# TODO: create a set from the resulting keys in the hashtable
+result = set(filter.keys())
+print(result)
+
+
+#---------------------------------------------------#
+# using a hashtable to count individual items
+
+# define a set of items that we want to count
+items = ['apple', 'pear', 'orange', 'banana', 'apple', 'orange',
+        'apple', 'pear', 'banana', 'orange', 'apple', 'kiwi',
+        'pear', 'apple', 'orange']
+
+# TODO: create a hashtable object to hold the items and counts
+counter = dict()
+
+# TODO: iterate over each item and increment the count for each one
+for item in items:
+    if (item in counter.keys()):
+        counter[item] += 1
+    else:
+        counter[item] = 1
+
+# print the results
+print(counter)
